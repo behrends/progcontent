66251e7ab86b26358d16ecf6a931287e3707329b
Erik Behrends <behrends@users.noreply.github.com>
2019-08-22 13:45:18 +0200

    Add Typography

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 
-export default () => <div>Hello world!</div>;
+export default () => (
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <h1>Module exportieren und importieren</h1>
+    <p>Hier geht es um export und import</p>
+  </div>
+);
