b6da348eb71ea718a14b5a805b7519e802b542e6
Erik Behrends <behrends@users.noreply.github.com>
2019-08-23 11:53:35 +0200

    Basic styling for prev and next links

--- src/components/prev-and-next.js
+++ src/components/prev-and-next.js
@@ -1,25 +1,54 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
+
+const PrevAndNextLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const SingleNextLink = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const LinkTitle = styled.h4`
+  color: darkgrey;
+  margin-bottom: 0;
+`;
 
 export default ({ prev = null, next = null }) => {
   if (!prev && !next) {
     return null;
   }
 
+  // display single next link at righter end if no prev link
+  // TODO: could we get rid of this with better styling?
+  if (!prev) {
+    return (
+      <SingleNextLink>
+        <Link to={next.link}>
+          <LinkTitle>Weiter</LinkTitle>
+          {next.title}
+        </Link>
+      </SingleNextLink>
+    );
+  }
+
   return (
-    <div>
+    <PrevAndNextLinks>
       {prev && (
         <Link to={prev.link}>
-          <h4>Zurück</h4>
+          <LinkTitle>Zurück</LinkTitle>
           {prev.title}
         </Link>
       )}
       {next && (
         <Link to={next.link}>
-          <h4>Weiter</h4>
+          <LinkTitle>Weiter</LinkTitle>
           {next.title}
         </Link>
       )}
-    </div>
+    </PrevAndNextLinks>
   );
 };
