98ef68d4e002811a2365a8fc9485f933114b8e73
Erik Behrends <behrends@users.noreply.github.com>
2019-08-23 11:56:28 +0200

    Add Home link to top of layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import 'prismjs/themes/prism-coy.css';
 
 export default ({ children }) => (
   <div style={{ margin: `3rem auto`, maxWidth: 740, padding: `0 1rem` }}>
+    <Link to="/">Inhalt</Link>
     {children}
   </div>
 );
