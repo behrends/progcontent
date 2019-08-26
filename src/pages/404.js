618fa7b300ebcedbf0bf319b35f3b5e0d798855c
Erik Behrends <behrends@users.noreply.github.com>
2019-08-26 15:32:01 +0200

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+export default () => (
+  <Layout>
+    <div>
+      <h1>Seite nicht gefunden</h1>
+      <p>Diese Seite gibt es leider nicht :-(</p>
+    </div>
+  </Layout>
+);
