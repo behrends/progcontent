c677a72bb6d5ea155e9ba4ea3abb01c94094bb7b
Erik Behrends <behrends@users.noreply.github.com>
2020-02-04 14:14:37 +0100

    Display Content heading

--- src/components/unit-list.js
+++ src/components/unit-list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link, graphql, useStaticQuery } from 'gatsby';
 
 export default ({ path }) => {
@@ -6,7 +6,9 @@ export default ({ path }) => {
   const data = useStaticQuery(graphql`
     query {
       allMdx(
-        filter: { frontmatter: { templateKey: { eq: "markdown-unit" } } }
+        filter: {
+          frontmatter: { templateKey: { eq: "markdown-unit" } }
+        }
         sort: { fields: fileAbsolutePath, order: ASC }
       ) {
         edges {
@@ -27,17 +29,24 @@ export default ({ path }) => {
   // TODO: check for feature to pass variables to static query
   // will this approach create bigger JS-builds delivered to browser?
   // https://github.com/gatsbyjs/gatsby/issues/10482
+
+  const edges = data.allMdx.edges.filter(
+    ({ node }) => node.fileAbsolutePath.indexOf(coursePath) !== -1
+  );
+  if (edges.length === 0) return null;
+
   return (
-    <ul>
-      {data.allMdx.edges
-        .filter(
-          ({ node }) => node.fileAbsolutePath.indexOf(coursePath) !== -1
-        )
-        .map(({ node }) => (
+    <>
+      <h3>Inhalt</h3>
+      <ul>
+        {edges.map(({ node }) => (
           <li key={node.id}>
-            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+            <Link to={node.fields.slug}>
+              {node.frontmatter.title}
+            </Link>
           </li>
         ))}
-    </ul>
+      </ul>
+    </>
   );
 };
