62f0bd465b1bf3e01e01e3d5343ceecce08b4387
Erik Behrends <behrends@users.noreply.github.com>
2020-02-12 20:54:24 +0100

    Legal and Copyright etc.

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,16 +6,34 @@ import 'prismjs/themes/prism-coy.css';
 
 export default ({ children, indexLink }) => (
   <div
-    style={{ margin: `3rem auto`, maxWidth: 1080, padding: `0 1rem` }}
+    style={{
+      margin: `0 3rem auto`,
+      maxWidth: 1080,
+      minHeight: '100vh',
+      display: 'flex',
+      flexDirection: 'column'
+    }}
   >
     <Helmet>
       <meta charSet="utf-8" />
       <title>Prog Content</title>
       <link rel="canonical" href="https://www.progcontent.com" />
     </Helmet>
-    <Link to="/">Startseite</Link>
-    &nbsp;&nbsp;{indexLink}
-    <br />
-    {children}
+    <header>
+      <Link to="/">Startseite</Link>
+      &nbsp;&nbsp;{indexLink}
+    </header>
+    <div style={{ flex: 1 }}>{children}</div>
+    <footer
+      style={{
+        marginTop: '1rem',
+        textAlign: 'right'
+      }}
+    >
+      &copy; {new Date().getFullYear()} &nbsp;
+      <a href="https://www.behrends.io">Erik Behrends</a>
+      &nbsp;&nbsp;&nbsp;
+      <Link to="/impressum">Impressum</Link>
+    </footer>
   </div>
 );
