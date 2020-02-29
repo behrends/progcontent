77dae6cd5ca39fc3efecfb610ac6d21addf010ed
Erik Behrends <behrends@users.noreply.github.com>
2020-02-29 11:43:50 +0100

    Add Codepen embed component

---
+++ src/components/codepen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+export default ({ id, height, defaultTabs }) => {
+  let theHeight = height || 300;
+  let theDefaultTabs = defaultTabs || 'css,result';
+  return (
+    <>
+      <Helmet>
+        <script src="https://static.codepen.io/assets/embed/ei.js" />
+      </Helmet>
+      <p
+        className="codepen"
+        data-height={theHeight}
+        data-theme-id="dark"
+        data-default-tab={theDefaultTabs}
+        data-user="behrends"
+        data-slug-hash={id}
+        style={{
+          height: `${theHeight}px`,
+          boxSizing: 'border-box',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          border: '2px solid',
+          margin: '1em 0',
+          padding: '1em'
+        }}
+        data-pen-title="Cascade"
+      ></p>
+    </>
+  );
+};
