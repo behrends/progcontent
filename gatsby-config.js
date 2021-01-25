241693591a1cb0751be36500aba93364a94bca48
Erik Behrends <behrends@users.noreply.github.com>
2021-01-25 16:10:36 +0100

    Upgrade packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,12 @@ module.exports = {
           },
           {
             resolve: `gatsby-remark-images`,
-            // It's important to specify the maxWidth (in pixels) of
-            // the content container as this plugin uses this as the
-            // base for generating different widths of each image.
-            maxWidth: 1080
-          }
-        ]
-      }
+            options: {
+              maxWidth: 1080,
+            },
+          },
+        ],
+      },
     },
     {
       resolve: `gatsby-plugin-typography`,
