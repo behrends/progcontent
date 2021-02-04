6069cd904f4a88c8e956bc224e67f7353e844f19
Erik Behrends <behrends@users.noreply.github.com>
2021-02-04 18:28:34 +0100

    Remove obsolete class in CourseCard

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 
 const CourseCard = ({id,link,title,description}) => {
   return (
-    <Link to={link} key={id} className="p-2 bg-white no-underline rounded-xl shadow-md hover:bg-indigo-100 transition duration-300 ease-in-out transform hover:scale-105">
+    <Link to={link} key={id} className="p-2 bg-white rounded-xl shadow-md hover:bg-indigo-100 transition duration-300 ease-in-out transform hover:scale-105">
       <h2 className="text-2xl">{title}</h2>
       <p>{description}</p>
     </Link>
