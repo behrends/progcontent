f8dc5d383263734a08d1bffe24bc3ce7f8f30958
Erik Behrends <behrends@users.noreply.github.com>
2021-02-04 18:28:04 +0100

    Remove obsolete div in CourseCard

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -3,12 +3,10 @@ import { Link } from 'gatsby';
 
 const CourseCard = ({id,link,title,description}) => {
   return (
-    <div key={id} className="p-2 bg-white rounded-xl shadow-md hover:bg-indigo-100 transition duration-300 ease-in-out transform hover:scale-105">
-      <Link to={link} className="no-underline">
-        <h2 className="text-2xl">{title}</h2>
-        <p>{description}</p>
-      </Link>
-    </div>
+    <Link to={link} key={id} className="p-2 bg-white no-underline rounded-xl shadow-md hover:bg-indigo-100 transition duration-300 ease-in-out transform hover:scale-105">
+      <h2 className="text-2xl">{title}</h2>
+      <p>{description}</p>
+    </Link>
   )
 }
 
