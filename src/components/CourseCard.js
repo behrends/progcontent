1e51ec4a243a60cb3dbfd9ee6f1d82507ae0e403
Erik Behrends <behrends@users.noreply.github.com>
2021-02-04 18:25:53 +0100

    Use transition ease-in-out for CourseCards

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -3,7 +3,7 @@ import { Link } from 'gatsby';
 
 const CourseCard = ({id,link,title,description}) => {
   return (
-    <div key={id} className="p-2 bg-white rounded-xl shadow-md hover:bg-indigo-100 transform hover:scale-105">
+    <div key={id} className="p-2 bg-white rounded-xl shadow-md hover:bg-indigo-100 transition duration-300 ease-in-out transform hover:scale-105">
       <Link to={link} className="no-underline">
         <h2 className="text-2xl">{title}</h2>
         <p>{description}</p>
