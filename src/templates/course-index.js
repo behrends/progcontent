29db1174f4c35acf285923d7e4b1519658bebe54
Erik Behrends <behrends@users.noreply.github.com>
2021-02-05 11:54:34 +0100

    Add WIP note to course index

--- src/templates/course-index.js
+++ src/templates/course-index.js
@@ -6,11 +6,17 @@ import UnitList from '../components/unit-list';
 
 const Course = ({ data: { mdx } }) => {
   const { title, description, wip } = mdx.frontmatter;
+  const wipNote = (
+    <p className="italic text-indigo-500">
+      An diesem Kurs wird noch gearbeitet
+    </p>
+  );
   return (
     <Layout>
       <div className="mx-auto prose prose-sm sm:prose lg:prose-lg xl:prose-2xl">
         <h1>{title}</h1>
         <h2>{description}</h2>
+        {wip && wipNote}
         <MDXRenderer>{mdx.body}</MDXRenderer>
         <UnitList path={mdx.fields.filePath} showStartButton={!wip} />
       </div>
