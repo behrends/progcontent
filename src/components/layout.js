d7728cfd005f49dcaac670de407a002742eea096
Erik Behrends <behrends@users.noreply.github.com>
2021-02-05 11:22:03 +0100

    Give header some blue

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,14 +6,13 @@ import './printLayout.css';
 import 'prismjs/themes/prism-coy.css';
 
 export default ({ children, indexLink }) => (
-  <div className="flex flex-col min-h-screen bg-gray-200"
-  >
+  <div className="flex flex-col min-h-screen bg-gray-50">
     <Helmet>
       <meta charSet="utf-8" />
       <title>Prog Content</title>
       <link rel="canonical" href="https://www.progcontent.com" />
     </Helmet>
-    <header className="mb-2 p-2 bg-gray-400">
+    <header className="mb-2 p-2 sm:text-lg md:text-xl text-white bg-indigo-500">
       <Link to="/">Prog Content</Link>
       &nbsp;&nbsp;{indexLink}
     </header>
