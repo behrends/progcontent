3e087c8775c652e2bcc12d69bb88225dc72b926f
Erik Behrends <behrends@users.noreply.github.com>
2021-07-07 11:19:46 +0200

    Enable server-side rendering on netlify

--- next.config.js
+++ next.config.js
@@ -1,2 +1,5 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js')
 module.exports = withNextra()
+// server side rendering on netlify
+// https://docs.netlify.com/configure-builds/common-configurations/next-js/
+module.exports.target = "serverless"
