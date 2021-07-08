b295f2b803cd012e0ca664d3240ce2455c92b8fb
Erik Behrends <behrends@users.noreply.github.com>
2021-07-08 17:19:38 +0200

    Dark/Light mode in editors

--- components/WebPlayground.js
+++ components/WebPlayground.js
@@ -1,6 +1,7 @@
 import Playground from '@agney/playground';
 /* Why is there a tabs import? https://github.com/agneym/playground#why-is-there-a-reacttabs-import*/
 import '@reach/tabs/styles.css';
+import { useTheme } from 'next-themes';
 
 const WebPlayground = ({
   markup = '',
@@ -9,11 +10,13 @@ const WebPlayground = ({
   defaultEditorTab = 'markup',
 }) => {
   const snippet = { markup, css, javascript };
+  const { theme } = useTheme();
   return (
     <Playground
       id="example"
       initialSnippet={snippet}
       defaultEditorTab={defaultEditorTab}
+      mode={theme}
     />
   );
 };
