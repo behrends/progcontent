ece92abbd79b0fbb4ac8a5057cf44dd43847ed78
Erik Behrends <behrends@users.noreply.github.com>
2021-07-08 13:32:12 +0200

    Update License and Copyright

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 ## Platform for learning various programming topics
 
 Built with [NextJS](https://nextjs.org/) and [Nextra](https://nextra.vercel.app/) by using Git/GitHub as a single source of truth, and [Netlify](https://www.netlify.com) for deployment.
+
+## Licenses 
+
+### Code
+
+[MIT](/LICENSE)
+
+### Content
+
+[CC BY NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)
+
+          
+
--- theme.config.js
+++ theme.config.js
@@ -9,7 +9,26 @@ export default {
   defaultMenuCollapsed: false,
   font: true,
   footer: true,
-  footerText: `© ${new Date().getFullYear()} Erik Behrends.`,
+  footerText: (
+    <span>
+      <abbr
+        title="Dieses Werk ist lizenziert unter einer Creative Commons Namensnennung-Nicht kommerziell 4.0 International Lizenz."
+        style={{ cursor: 'help', marginRight: '10px' }}
+      >
+        <a
+          rel="license"
+          href="https://creativecommons.org/licenses/by-nc/4.0/"
+        >
+          CC BY NC 4.0
+        </a>
+      </abbr>
+      <span>{`© ${new Date().getFullYear()} Erik Behrends`}</span>
+      <a href="/impressum" style={{ marginLeft: '10px' }}>
+        Impressum
+      </a>
+    </span>
+  ),
+  // footerText: `© ${new Date().getFullYear()} Erik Behrends.`,
   footerEditLink: 'Diese Seite bearbeiten.',
   logo: <span className="font-extrabold">Prog Content</span>,
   head: (
