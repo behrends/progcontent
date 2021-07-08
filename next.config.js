62b6b56302250347f41c4e6fedab4cc63fca3fc3
Erik Behrends <behrends@users.noreply.github.com>
2021-07-08 16:23:39 +0200

    Disable stork search

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
-  unstable_stork: true,
+  unstable_stork: false,
 });
 module.exports = withNextra();
 // server side rendering on netlify
