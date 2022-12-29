dd2c0bcc47b5d8f52f71c7a4535eda8c96bbf136
Erik Behrends <behrends@users.noreply.github.com>
2022-12-29 17:24:54 +0100

    Upgrade to NextJS 13, React 18, Nextra 2

--- theme.config.js
+++ theme.config.js
@@ -1,64 +1,79 @@
 export default {
-  github: 'https://github.com/behrends/progcontent', // project repo
-  docsRepositoryBase: 'https://github.com/behrends/progcontent', // docs repo
-  titleSuffix: '',
-  nextLinks: true,
-  prevLinks: true,
-  search: true,
-  unstable_stork: false,
-  darkMode: true,
-  defaultMenuCollapsed: true,
-  feedbackLink: () => 'Fragen / Rückmeldung auf GitHub →',
-  feedbackLabels: 'feedback',
-  floatTOC: true,
-  font: true,
-  footer: true,
-  footerEditLink: 'Diese Seite bei GitHub →',
-  footerText: (
-    <span>
-      <abbr
-        title="Dieses Werk ist lizenziert unter einer Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz."
-        style={{ cursor: 'help', marginRight: '10px' }}
-      >
-        <a
-          rel="license"
-          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-          target="_blank"
-        >
-          CC BY-NC-SA 4.0
-        </a>
-      </abbr>
-      <span>
-        {`© ${new Date().getFullYear()} `}
-        <a
-          rel="author"
-          href="https://www.behrends.io"
-          target="_blank"
-        >
-          Erik Behrends
-        </a>
-      </span>
-      <a href="/impressum" style={{ marginLeft: '10px' }}>
-        Impressum
-      </a>
-    </span>
-  ),
-  logo: <span className="font-extrabold">Prog Content</span>,
+  docsRepositoryBase: 'https://github.com/behrends/progcontent',
+  editLink: {
+    text: 'Diese Seite bei GitHub →',
+  },
+  feedback: {
+    content: undefined,
+  },
+  footer: {
+    text() {
+      return (
+        <span>
+          <abbr
+            title="Dieses Werk ist lizenziert unter einer Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International Lizenz."
+            style={{ cursor: 'help', marginRight: '10px' }}
+          >
+            <a
+              rel="license"
+              href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
+              target="_blank"
+            >
+              CC BY-NC-SA 4.0
+            </a>
+          </abbr>
+          <span>
+            {`© ${new Date().getFullYear()} `}
+            <a
+              rel="author"
+              href="https://www.behrends.io"
+              target="_blank"
+            >
+              Erik Behrends
+            </a>
+          </span>
+          <a href="/impressum" style={{ marginLeft: '10px' }}>
+            Impressum
+          </a>
+        </span>
+      );
+    },
+  },
+  gitTimestamp: undefined,
   head: (
     <>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      />
+      <meta name="msapplication-TileColor" content="#fff" />
       <meta httpEquiv="Content-Language" content="de" />
+      <meta name="description" content="Workshops Notizen" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@behrends" />
       <meta
-        name="description"
-        content="Prog Content - Eigenständig Programmieren lernen mit interaktiven Kursen"
+        property="og:title"
+        content="Prog Content - Programmieren lernen"
       />
       <meta
-        name="og:title"
-        content="Prog Content - Eigenständig Programmieren lernen mit interaktiven Kursen"
+        property="og:description"
+        content="Prog Content - Programmieren lernen"
       />
+      <meta name="apple-mobile-web-app-title" content="Progcontent" />
     </>
   ),
+  logo: <span className="font-bold">Progcontent</span>,
+  project: {
+    link: 'https://github.com/behrends/progcontent',
+  },
+  search: {
+    placeholder: 'Inhalte durchsuchen…',
+  },
+  sidebar: {
+    defaultMenuCollapseLevel: 0,
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s',
+    };
+  },
+  toc: {
+    title: 'Auf dieser Seite',
+  },
 };
